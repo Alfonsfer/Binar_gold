a0873883a778116a9aea0a364f3321ba4faba118
Alfonsfer <alfonsus.christofer@gmail.com>
2023-01-26 22:16:47 +0700

    edit model index

--- database/models/index.js
+++ database/models/index.js
@@ -1,38 +1,49 @@
-'use strict';
+const User = require('./users.model')
+const Item = require('./items.model')
+const Order = require('./orders.model')
+const OrderItem = require('./order-item.model')
+const sequelize = require('./sequelize')
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
-const db = {};
+User.hasMany(Item, {
+  as: 'item'
+})
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+User.hasMany(Order, {
+  as: 'order'
+})
+
+Item.belongsTo(User, {
+  as: 'user',
+  foreignKey: 'user_id',
+})
+
+Item.hasMany(OrderItem, {
+  as: 'order-item'
+})
+
+Order.belongsTo(User, {
+  as: 'user',
+  foreignKey: 'user_id'
+})
+
+Order.hasMany(OrderItem, {
+  as: 'order-item'
+})
+
+OrderItem.belongsTo(Order, {
+  as: 'order',
+  foreignKey: 'order_id'
+})
+
+OrderItem.belongsTo(Item, {
+  as: 'item',
+  foreignKey: 'item_id'
+})
+
+module.exports = {
+  User,
+  Item,
+  Order,
+  OrderItem,
+  sequelize
 }
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
